54a7c93d5f8e1a5ffbfa60c477a2632e13259c5c
Ahmed2120 <67471054+Ahmed2120@users.noreply.github.com>
2020-12-03 22:50:35 +0200

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-export AUTH0_DOMAIN="dev-fa25pxj9.us.auth0.com"
-export API_AUDIENCE="capstone"
-export ALGORITHMS=['RS256']
-export FLASK_APP=app.py
-export FLASK_ENV=development
-export SQLALCHEMY_DATABASE_URI="postgresql://postgres:ahmedpost@localhost:5432/casting"
